2f4f40536f51c283d4b3d1bbd7710299f78368b2
Lucas Vieira <lucas@vieira.io>
2015-02-28 11:07:25 -0300

    clean version of 4550 cdc

---
+++ usb/dvd-cdc-pic18f4550/src/dp_usb/usb_stack_globals.h
@@ -0,0 +1,19 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+// JTR V0.2a 26th Jan 2012 
+
+typedef unsigned char BYTE;
+
+// =================== GLOBAL HEADERS
+#include "prj_usb_config.h" // from parent folder.
+#include "../dp_usb/usb_stack.h"
+#include "../dp_usb/cdc.h"
+
+// #include <string.h>
+
+#define NULL 0
+typedef unsigned short int size_t;
+
+
+#endif
